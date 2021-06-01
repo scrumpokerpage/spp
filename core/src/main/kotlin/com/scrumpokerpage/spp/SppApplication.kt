089ae7c54ed67a696471e57ee10ae4c92bb31a96
Lothar Buchholz <lothar@lothar-buchholz.de>
2021-06-01 18:37:38 +0200

    NO-TICKET: Setup base frameworks
    * SpringBoot 2.5.0
    * Kotlin 1.5.0
    * LiquiBase 4.3.5
    * H2 1.4
    * JDK 16

---
+++ core/src/main/kotlin/com/scrumpokerpage/spp/SppApplication.kt
@@ -0,0 +1,11 @@
+package com.scrumpokerpage.spp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SppApplication
+
+fun main(args: Array<String>) {
+	runApplication<SppApplication>(*args)
+}
---
+++ core/src/test/kotlin/com/scrumpokerpage/spp/SppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.scrumpokerpage.spp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SppApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
